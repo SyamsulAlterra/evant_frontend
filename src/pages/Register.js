40f313ae015152967b4b8c1330e808988256a8ee
Syamsul <syamsul@alterra.id>
2019-09-12 16:01:48 +0700

    register page can connect

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Button from "../components/Button";
+import Axios from "axios";
 
 class Register extends React.Component {
   constructor(props) {
@@ -9,7 +10,10 @@ class Register extends React.Component {
       username: "",
       email: "",
       password: "",
-      confirmPassword: ""
+      confirmPassword: "",
+      phone: "",
+      address: "",
+      gender: ""
     };
   }
 
@@ -38,8 +42,44 @@ class Register extends React.Component {
     this.setState({ confirmPassword: inputConfirmPassword });
   };
 
-  handleClick = () => {
+  handlePhone = e => {
+    let inputPhone = e.target.value;
+    this.setState({ phone: inputPhone });
+  };
+
+  handleAddress = e => {
+    let inputAddress = e.target.value;
+    this.setState({ address: inputAddress });
+  };
+
+  handleGender = e => {
+    let inputGender = e.target.value;
+    console.log(inputGender);
+    if (inputGender === "true" || inputGender === "") {
+      this.setState({ gender: true });
+    } else if (inputGender === "false") {
+      this.setState({ gender: false });
+    }
+  };
+
+  handleClick = async () => {
     console.log(this.state);
+    let config = {
+      url: "http://0.0.0.0:5000/api/users/register",
+      method: "post",
+      data: {
+        username: this.state.username,
+        email: this.state.email,
+        password: this.state.password,
+        gender: this.state.gender,
+        fullname: this.state.name,
+        address: this.state.address,
+        phone: this.state.phone
+      }
+    };
+
+    let register = await Axios(config);
+    console.log(register);
   };
 
   render() {
@@ -81,9 +121,41 @@ class Register extends React.Component {
                   placeholder="Confirm password"
                   onChange={this.handleConfirmPassword}
                 />
+                <input
+                  type="text"
+                  className="m-2"
+                  placeholder="Phone"
+                  onChange={this.handlePhone}
+                />
+                <textarea
+                  type="text"
+                  className="m-2"
+                  placeholder="Address"
+                  onChange={this.handleAddress}
+                />
+                <br />
+                <div className="text-left mx-5 px-4">
+                  <input
+                    type="radio"
+                    name="gender"
+                    value={true}
+                    onChange={this.handleGender}
+                    // checked="checked"
+                  />
+                  Male
+                  <br />
+                  <input
+                    type="radio"
+                    name="gender"
+                    value={false}
+                    onChange={this.handleGender}
+                    // checked="checked"
+                  />
+                  Female <br />
+                </div>
               </form>
               <Button
-                className="mt-3"
+                className="mt-5"
                 buttonName="Register"
                 handleClick={this.handleClick}
               ></Button>
