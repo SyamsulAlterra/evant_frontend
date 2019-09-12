c40013a818fb01d9eef53e09c4beecdedf031881
zulyanor <zulyano@alterra.id>
2019-09-12 21:17:56 +0700

    added event history section

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -7,7 +7,8 @@ class Events extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      listEvent: []
+      listEvent: [],
+      pastEvent: []
     };
   }
 
@@ -20,9 +21,15 @@ class Events extends React.Component {
     await axios
       .get(this.props.baseUrl + "events/ongoing", config)
       .then(response => {
-        console.log(response.data);
         this.setState({ listEvent: response.data });
-        console.log("state list event", this.state.listEvent);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+    await axios
+      .get(this.props.baseUrl + "events/history", config)
+      .then(response => {
+        this.setState({ pastEvent: response.data });
       })
       .catch(error => {
         console.log(error);
@@ -31,40 +38,79 @@ class Events extends React.Component {
 
   render() {
     return (
-      <div class="accordion" id="accordionExample">
-        <div class="card">
-          <div class="card-header" id="headingOne">
-            <h2 class="mb-0">
-              <button
-                class="btn btn-link"
-                type="button"
-                data-toggle="collapse"
-                data-target="#collapseOne"
-                aria-expanded="true"
-                aria-controls="collapseOne"
-              >
-                Ongoing Events
-              </button>
-            </h2>
-          </div>
+      <div className="eventContent">
+        <div className="accordion ongoingEvent" id="accordionExample">
+          <div className="card">
+            <div className="card-header" id="headingOne">
+              <h2 className="mb-0">
+                <button
+                  className="btn btn-link"
+                  type="button"
+                  data-toggle="collapse"
+                  data-target="#collapseOne"
+                  aria-expanded="true"
+                  aria-controls="collapseOne"
+                >
+                  Ongoing Events
+                </button>
+              </h2>
+            </div>
 
-          <div
-            id="collapseOne"
-            class="collapse"
-            aria-labelledby="headingOne"
-            data-parent="#accordionExample"
-          >
-            {this.state.listEvent.map((value, index) => {
-              return (
-                <div className="border">
-                  <CollapseEvent
-                    id={value.event_id}
-                    eventName={value.event_name}
-                    category={value.category}
-                  />
-                </div>
-              );
-            })}
+            <div
+              id="collapseOne"
+              className="collapse"
+              aria-labelledby="headingOne"
+              data-parent="#accordionExample"
+            >
+              {this.state.listEvent.map(value => {
+                return (
+                  <div className="border">
+                    <CollapseEvent
+                      id={value.event_id}
+                      eventName={value.event_name}
+                      category={value.category}
+                    />
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+        <div className="accordion pastEvent" id="accordionExample">
+          <div className="card">
+            <div className="card-header" id="headingOne">
+              <h2 className="mb-0">
+                <button
+                  className="btn btn-link"
+                  type="button"
+                  data-toggle="collapse"
+                  data-target="#collapseTwo"
+                  aria-expanded="true"
+                  aria-controls="collapseTwo"
+                >
+                  Events History
+                </button>
+              </h2>
+            </div>
+
+            <div
+              id="collapseTwo"
+              className="collapse"
+              aria-labelledby="headingTwo"
+              data-parent="#accordionExample"
+            >
+              {this.state.pastEvent.map(value => {
+                return (
+                  <div className="border">
+                    <CollapseEvent
+                      id={value.event_id}
+                      eventName={value.event_name}
+                      category={value.category}
+                    />
+                  </div>
+                );
+              })}
+            </div>
           </div>
         </div>
       </div>
