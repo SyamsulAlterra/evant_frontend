f3059eca2b0a7281611b9192a4a168207e83c67a
RadenMahar <panji@alterra.id>
2019-09-15 19:33:42 +0700

    styling header

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,11 +9,11 @@ class Header extends React.Component {
     return (
       <div className="container-fluid header">
         <div className="row justify-content-center">
-          <div className="col bg-info text-center pt-2 pb-2 "></div>
-          <div className="col-3 bg-white text-left pt-2 pb-2 header-style">
+          <div className="col bg-light text-center pt-2 pb-2 "></div>
+          <div className="col-3 bg-light text-center pt-2 pb-2 header-style">
             <h4>Evant</h4>
           </div>
-          <div className="col bg-info text-center pt-2 pb-2"></div>
+          <div className="col bg-light text-center pt-2 pb-2"></div>
         </div>
       </div>
     );
