a33341fadc4bc61c3e995b97fa180c170208014a
RadenMahar <panji@alterra.id>
2019-09-17 02:39:22 +0700

    just commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "../images/linkedin_banner_image.png";
+import logo from "../images/twitter_header_photo_1.png";
 
 class Header extends React.Component {
   handleClick = () => {
@@ -11,9 +11,13 @@ class Header extends React.Component {
       <div className="container-fluid header">
         <div className="row justify-content-center h-100">
           <div className="col text-center pt-2 pb-2 "></div>
-          <div className="col-3 text-center pt-2 pb-2 header-style h-100">
-            <img src={logo} alt="" height="100%" />
+          <div className="row ustify-content-center h-100">
+            {" "}
+            <div className="col-3 text-center pt-2 pb-2 header-style h-100">
+              <img src={logo} alt="" height="100%" />
+            </div>
           </div>
+
           <div className="col text-center pt-2 pb-2"></div>
         </div>
       </div>
