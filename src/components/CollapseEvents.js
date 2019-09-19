f1b97565f4983be2670f5f9d4ec28c45314680a6
zulyanor <zulyano@alterra.id>
2019-09-19 17:45:45 +0700

    list event

--- src/components/CollapseEvents.js
+++ src/components/CollapseEvents.js
@@ -5,17 +5,16 @@ class CollapseEvents extends React.Component {
   render() {
     console.log(this.props.id);
     return (
-      <div className="col-8 text-center my-3 border shadow p-3 rounded">
-        Event Name: {this.props.eventName} <br />
-        <br />
+      <div className="col-12 text-left my-3 border shadow p-3 rounded">
+        <div className="text-center">
+          <b>{this.props.eventName}</b> <br />
+        </div>
         Category: {this.props.category} <br />
-        <br />
-        Created by: {this.props.creatorName}
-        <br />
-        <br />
-        <div className="button-detail">
+        Created by: <b>@{this.props.creatorName}</b>
+        <div className="button-detail text-right toEventDetails">
           <Link to={"/transition/" + this.props.id.toString()}>
-            <button className="btn eventDetailButton">See Event Detail</button>
+            {/* <button className="btn eventDetailButton">See Event Detail</button> */}
+            See details..
           </Link>
         </div>
       </div>
